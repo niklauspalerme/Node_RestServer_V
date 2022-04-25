080bd3d268b62ae2dec01035135907699a8a3373
Palermo Rodriguez <nicolas.palermo@kcc.com>
2022-04-25 16:26:31 -0300

    13) Búsquedas en base de datos
    Creando las condiciones y funcionabilidad para el controlador "buscar"

--- src/controllers/buscar.js
+++ src/controllers/buscar.js
@@ -1,12 +1,35 @@
 /////////////////////////////////////////////////////////////
-// Importaciones
+// Importaciones o Constantes
 
+const { ObjectId } = require('mongoose').Types;
+const { Usuarios } = require('../models/usuario');
 
+const coleccionesPermitidas = [
+    'usuarios',
+    'productos',
+    'categorias',
+    'roles'
+]
+
+const buscarUsuarios = async (termino = '', res) =>{
+
+    const esMongoId = ObjectId.isValid(termino)
+
+    console.log("Aja");
+
+    if (esMongoId){
+        const usuario = await Usuarios.findById(termino)
+        return res.json({
+           "msg": "GET /api/buscar/:coleccion/:termino",
+           usuario
+        })
+    }
+
+}
 
 
 /////////////////////////////////////////////////////////////
 // Funciones del Buscar
-
 
 
 const buscar = (req,res) => {
@@ -15,12 +38,36 @@ const buscar = (req,res) => {
 
         console.log("GET /api/buscar/:coleccion/:termino")
 
+        const {coleccion, termino} = req.params;
 
-        res.json({
-           "msg": "GET /api/buscar/:coleccion/:termino"
-        })
+        console.log(coleccion);
+
+        if (!coleccionesPermitidas.includes(coleccion))
+            return  res.status(400).json({
+                "msg": `The coleccion allowed are: ${coleccionesPermitidas}`
+             })
+
+        switch (coleccion) {
+            case 'productos':
+                
+                break;
+            case 'categorias':
+                
+                break;
+                        
+            case 'usuarios':
+                buscarUsuarios(termino, res)
+                break;
+
+            default:
+                return res.status(500).json({
+                    "msg": "Internal Server Error - Controllers/buscar"
+                })
+        }
+
        
    } catch (error) {
+       console.log(error);
         res.status(500).json({
             "msg": error
         })
